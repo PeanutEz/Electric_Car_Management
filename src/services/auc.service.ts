0b1bc816985dbad4b2973b4df3e0c619a9d79437
LacPham <phamgialac10a10@gmail.com>
2025-10-30 15:48:49 +0700

    Lac commit lai

--- src/services/auc.service.ts
+++ src/services/auc.service.ts
@@ -1,7 +1,7 @@
 import pool from '../config/db';
 import { Auction } from '../models/auction.model';
 
-export async function getAllAuctions(): Promise<Auction[]> {
+export async function getAllAuctions() {
 	const [rows] = await pool.query('SELECT a.*,p.image, p.title, p.address as location FROM auctions a inner join products p on a.product_id = p.id  lIMIT 10');
 	const [totalAuctions]: any = await pool.query('SELECT COUNT(*) as total_auctions FROM auctions');
 	return {
